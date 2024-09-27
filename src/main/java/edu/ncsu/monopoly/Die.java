e7e6826f9d86b6df9c585d296a95af9106e71c89
aitor <aitor@10.245.47.191>
2024-09-27 11:18:30 +0200

    Dado arreglo

--- src/main/java/edu/ncsu/monopoly/Die.java
+++ src/main/java/edu/ncsu/monopoly/Die.java
@@ -1,7 +1,10 @@
 package edu.ncsu.monopoly;
 
+import java.util.Random;
+
 public class Die {
+	Random random = new Random();
 	public int getRoll() {
-		return (int)(Math.random() * 6) + 1;
+		return (int)(random.nextInt(6)) + 1;
 	}
 }
